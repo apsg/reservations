b3439a8da70646f1932d4bf3a82d1773c551809c
Unknown <szymon.gackowski@gmail.com>
2019-09-08 21:16:41 +0200

    tests

---
+++ resources/js/components/DatesRow.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+
+        <div>{{ range.start }} : {{ range.end }}</div>
+
+        <range-selector
+            :start-date.sync="range.start"
+            :end-date.sync="range.end"></range-selector>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DatesRow",
+
+        props: {
+            dates: {
+                required: true,
+                type: Array
+            }
+        },
+
+        data() {
+            return {
+                range: {
+                    start: null,
+                    end: null,
+                }
+            };
+        },
+
+        methods: {
+
+
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .date-box {
+        width: 30px;
+        height: 30px;
+        margin: 1px;
+        border: 1px solid #333333;
+        display: inline-block;
+        transition: 0.3s;
+
+        &:hover {
+            background-color: lightblue;
+        }
+    }
+</style>
